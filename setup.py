bc04fc36cf8543af2c3a22fe36b11cd25e67c09c
Alexey Dubrov <nevermind1025@gmail.com>
2018-07-18 13:29:04 +0300

    upd to python 3.7

---
+++ setup.py
@@ -0,0 +1,3 @@
+from shutil import copyfile
+import os
+copyfile('./timer.py', os.path.expanduser('~/timer.py'))
--- timer.py
+++ timer.py
@@ -1,69 +1,67 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 import argparse
 import configparser
+from dataclasses import dataclass
+from dataclasses import field
 from datetime import datetime, date, timedelta as td
+from typing import List, Tuple, Dict
 
 import requests
+import urllib3
 from requests.adapters import HTTPAdapter
 from requests.auth import HTTPBasicAuth
 
 
+@dataclass(order=True)
 class Time(object):
-    format = '''{sign}{day}{hour:02}:{minute:02}'''
+    seconds: float = field(compare=True, default=0.0)
 
-    def __init__(self, time=None):
-        """Args:
-                time (int) time span in seconds
-                time (str) time span in format HH:MM
-                time (tuple) (hh,mm)
-                time (Time)
-        """
-        if time is None:
-            self.time = 0.0
-        elif isinstance(time, (int, float)):
-            self.time = float(time)
-        elif type(time) is str:
-            self.time = float(time[:2]) * 3600.0 + float(time[3:]) * 60.0
-        elif type(time) is tuple:
-            self.time = time[0] * 3600.0 + time[1] * 60.0
-        elif type(time) is td:
-            self.time = time.total_seconds()
-        elif type(time) is Time:
-            self.time = time.time
-        else:
-            raise TypeError('Time must be integer number of seconds or string in format HH:MM')
+    @classmethod
+    def from_string(cls, string):
+        seconds = float(string[:2]) * 3600.0 + float(string[3:]) * 60.0
+        return Time(seconds)
 
-    def __repr__(self):
-        negative = self.time < 0
-        total_seconds = round(abs(self.time))
+    @classmethod
+    def from_params(cls, hours=0, minutes=0, seconds=0):
+        seconds = hours * 3600 + minutes * 60 + seconds
+        return Time(seconds)
 
-        msec = 60
-        hsec = 60 * msec
-        dsec = 24 * hsec
+    def __format__(self, t_format=None):
+        if not t_format:
+            t_format = '''{sign}{day}{hour:02}:{minute:02}'''
+        negative = self.seconds < 0
+        total_seconds = round(abs(self.seconds))
 
-        day = total_seconds // dsec
-        total_seconds -= day * dsec
+        m_sec = 60
+        h_sec = 60 * m_sec
+        d_sec = 24 * h_sec
 
-        hour = total_seconds // hsec
-        total_seconds -= hour * hsec
+        day = total_seconds // d_sec
+        total_seconds -= day * d_sec
 
-        minute = total_seconds // msec
-        total_seconds -= minute * msec
+        hour = total_seconds // h_sec
+        total_seconds -= hour * h_sec
+
+        minute = total_seconds // m_sec
+        total_seconds -= minute * m_sec
 
         second = total_seconds
 
-        repr = Time.format.format(sign='-' if negative else '',
-                                  day=f'{day} day ' if day > 0 else '',
-                                  hour=hour,
-                                  minute=minute,
-                                  second=second)
-        return repr
+        repr_str = t_format.format(sign='-' if negative else '',
+                                   day=f'{day} day ' if day > 0 else '',
+                                   hour=hour,
+                                   minute=minute,
+                                   second=second)
+        return repr_str
+
+    def __repr__(self):
+        return self.__format__('''{sign}{day}{hour:02}:{minute:02}''')
 
     def __add__(self, other):
         if other == 0:
             return self
-        return Time(self.time + other.time)
+        return Time(self.seconds + other.seconds)
 
     def __radd__(self, other):
         if type(other) is int:
@@ -72,75 +70,81 @@ class Time(object):
             return self.__add__(other)
 
     def __sub__(self, other):
-        return self.__add__(Time(-other.time))
-
-    def __lt__(self, other):
-        return self.time < other.time
-
-    def __le__(self, other):
-        return self.time <= other.time
+        return self.__add__(Time(-other.seconds))
 
 
+class TermColor:
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    PURPLE = '\033[95m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+    NORM = END
+
+
+@dataclass
 class Entry(object):
-    def __init__(self, entry_id, billable, spent, note=''):
-        self.id = entry_id
-        self.billable = billable
-        self.spent = spent
-        self.note = note
+    id: int
+    billable: bool
+    spent: Time
+    note: str = ''
 
 
-class TicketingSystem(object):
-    def prepare_url(self):
-        raise (Exception, 'Not implemented')
+@dataclass
+class TicketingSystem:
+    config: configparser.RawConfigParser
+    report_date: datetime
+    offset: int
+    auth: Tuple[str] = field(init=False)
+    params: Dict[str, str] = field(init=False)
+    url: str = field(init=False)
+    api_url: str = field(init=False)
+    entries: List[Entry] = field(init=False)
+    entry_url: str = field(init=False)
+    max_retries: int = field(init=False, default=5)
+    timeout: int = field(init=False, default=5)
+
+    def __str__(self):
+        res = []
+        for entry in self.entries:
+            res.append(
+                f'''{self.entry_url}{entry.id}'''
+                f'''\n\t{'Bill' if entry.billable else 'Free'}: {entry.spent} {entry.note}''')
+        return '\n'.join(res)
+
+    def __repr__(self):
+        return __name__ + self.__str__()
 
     def get_json(self):
         try:
             s = requests.Session()
             s.mount('https://', HTTPAdapter(max_retries=self.max_retries))
             ans = s.get(self.api_url, params=self.params, auth=self.auth, timeout=self.timeout)
-        except requests.ConnectTimeout:
+            return ans.json()
+        except urllib3.exceptions.ConnectTimeoutError:
             print('Connection timeout...')
-            raise
-        return ans.json()
 
     def parse_json(self):
         raise (Exception, 'Not implemented')
 
-    def get_billable(self):
-        time = Time(sum(i.spent for i in self.entries if i.billable))
+    def get_bill(self):
+        time = Time(sum(i.spent.seconds for i in self.entries if i.billable))
         return time
 
-    def get_notbillable(self):
-        time = Time(sum(i.spent for i in self.entries if not i.billable))
+    def get_free(self):
+        time = Time(sum(i.spent.seconds for i in self.entries if not i.billable))
         return time
 
     def get_total(self):
-        return self.get_billable() + self.get_notbillable()
-
-    def __init__(self, config, report_date):
-        self.config = config
-        self.report_date = report_date
-        self.auth = None
-        self.params = None
-        self.url = None
-        self.api_url = None
-        self.max_retries = 5
-        self.timeout = 3
-        self.entries = []
-        self.entry_url = None
-
-    def __repr__(self):
-        res = []
-        for entry in self.entries:
-            res.append(
-                f'''{self.entry_url}{entry.id}'''
-                f'''\n\t{'Billable' if entry.billable else 'Not billable'}: {entry.spent} {entry.note}''')
-        return '\n'.join(res)
+        return self.get_bill() + self.get_free()
 
 
+@dataclass
 class Freshesk(TicketingSystem):
-    def __init__(self, config, report_date, offset):
-        TicketingSystem.__init__(self, config, report_date)
+    def __post_init__(self):
         self.report_date -= td(hours=self.config.getint('freshdesk', 'tz_shift')) + td(seconds=1)
         self.auth = (self.config.get('freshdesk', 'api_key'), 'X')
         self.params = {'agent_id': self.config.get('freshdesk', 'agent_id'),
@@ -149,48 +153,53 @@ class Freshesk(TicketingSystem):
         self.url = self.config.get('freshdesk', 'url')
         self.api_url = self.url + '/api/v2/time_entries'
         self.entry_url = self.url + '/a/tickets/'
-        self.entries = [Entry(entry_id=i.get('ticket_id'),
+        self.json = self.get_json()
+        self.data = sorted(self.json, key=lambda k: (k.get('ticket_id'), k.get('updated_at'))) if self.json else []
+        self.entries = [Entry(id=i.get('ticket_id'),
                               billable=i.get('billable'),
-                              spent=Time(i.get('time_spent')),
+                              spent=Time.from_string(i.get('time_spent')),
                               note=i.get('note'))
-                        for i in sorted(self.get_json(),
-                                        key=lambda k: (k.get('ticket_id'), k.get('updated_at')))]
+                        for i in self.data]
 
 
+@dataclass
 class TeamWork(TicketingSystem):
-    def __init__(self, config_path, report_date, offset):
-        TicketingSystem.__init__(self, config_path, report_date)
+    def __post_init__(self):
         self.auth = HTTPBasicAuth(self.config.get('teamwork', 'api_key'), 'X')
         self.url = self.config.get('teamwork', 'url')
         self.api_url = self.url + '/time_entries.json'
-        self.params = params = {
+        self.entry_url = self.url + '/#tasks/'
+        self.params = {
             'userId': self.config.get('teamwork', 'agent_id'),
             'fromdate': self.report_date.strftime('%Y%m%d'),
             'todate': self.report_date.strftime('%Y%m%d')
         }
-        self.entries = [Entry(entry_id=i.get('todo-item-id'),
-                              spent=(i.get('hours'), i.get('minutes')),
+        self.json = self.get_json()
+        self.data = sorted(self.get_json().get('time-entries')) if self.json else []
+        self.entries = [Entry(id=i.get('todo-item-id'),
+                              spent=(Time(int(i.get('hours')) * 3600 + int(i.get('minutes')) * 60)),
                               billable=(i.get('isbillable') == 1),
                               note=i.get('project-name'))
-                        for i in sorted(self.get_json().get('time-entries'))]
+                        for i in self.data]
 
 
+@dataclass
 class Jira(TicketingSystem):
-    def __init__(self, config_path, report_date, offset):
-        TicketingSystem.__init__(self, config_path, report_date)
+    def __post_init__(self):
         jira_login = self.config.get('jira', 'login')
         jira_pass = self.config.get('jira', 'password')
         self.auth = HTTPBasicAuth(jira_login, jira_pass)
         self.url = self.config.get('jira', 'url')
         self.api_url = self.url + '/rest/api/2/search'
         self.entry_url = self.url + '/browse/'
-        json = requests.post(self.api_url,
-                             headers={"Content-Type": "application/json"},
-                             json={
-                                 'jql': f'''worklogAuthor = {jira_login} AND worklogDate = {self.report_date.strftime('%Y-%m-%d')}''',
-                                 "fields": ["key"],
-                                 "maxResults": 1000},
-                             auth=HTTPBasicAuth(jira_login, jira_pass)).json()
+        json = requests.post(
+            self.api_url,
+            headers={'Content-Type': 'application/json'},
+            json={
+                'jql': f'''worklogAuthor = {jira_login} AND worklogDate = {self.report_date.strftime('%Y-%m-%d')}''',
+                'fields': ['key'],
+                'maxResults': 1000},
+            auth=HTTPBasicAuth(jira_login, jira_pass)).json()
         entries = []
         for issue in json['issues']:
             ans = requests.get(f'{self.url}/rest/api/2/issue/' +
@@ -200,7 +209,7 @@ class Jira(TicketingSystem):
                 if (worklog['author']['name'] == jira_login and
                         worklog['started'].split('T')[0] == report_date.strftime('%Y-%m-%d')):
                     time_spent = int(worklog.get('timeSpentSeconds'))
-                    entries.append(Entry(entry_id=issue.get('key'),
+                    entries.append(Entry(id=issue.get('key'),
                                          billable=False,
                                          spent=Time(time_spent),
                                          note=worklog.get('comment')))
@@ -208,8 +217,8 @@ class Jira(TicketingSystem):
 
 
 parser = argparse.ArgumentParser(description='Simple time tracker for Freshdesk')
-parser.add_argument('config_path', action='store', type=str, nargs='?', help='Path to config')
 parser.add_argument('offset', action='store', type=int, nargs='?', help='Offset in days from today')
+parser.add_argument('config_path', action='store', type=str, nargs='?', help='Path to config')
 parser.set_defaults(offset=0, config_path='./config.ini')
 
 args = parser.parse_args()
@@ -219,26 +228,44 @@ config.read(args.config_path)
 offset = args.offset
 report_date = datetime.combine(date.today(), datetime.min.time()) - td(days=offset)
 
-print(report_date.strftime('%d %b %Y'))
-print()
+date_color = TermColor.RED if report_date.weekday() in (5, 6) else TermColor.END
 
-fd = Freshesk(config, report_date, offset)
-print(fd)
-ji = Jira(config, report_date, offset)
-print(ji)
-tw = TeamWork(config, report_date, offset)
-print(tw)
+print(f'''Time records for {report_date.strftime(f'{date_color}%a %d %b %Y{TermColor.END}')}\n''')
 
-workday_begin = Time(config.get('global', 'workday_begin'))
-workday_end = Time(config.get('global', 'workday_end'))
-launch_begin = Time(config.get('global', 'launch_begin'))
-launch_end = Time(config.get('global', 'launch_end'))
+params = (config, report_date, offset)
+
+print('\rGetting Freshesk...', end='')
+fd = Freshesk(*params)
+print('\rGetting Jira...    ', end='')
+ji = Jira(*params)
+print('\rGetting TeamWork...', end='')
+tw = TeamWork(*params)
+print('\r                   ', end='\r')
+
+
+def print_if_not_empty(ts: TicketingSystem):
+    if ts.entries:
+        print(ts)
+
+
+print_if_not_empty(fd)
+print_if_not_empty(ji)
+print_if_not_empty(tw)
+
+workday_begin = Time.from_string(config.get('global', 'workday_begin'))
+workday_end = Time.from_string(config.get('global', 'workday_end'))
+launch_begin = Time.from_string(config.get('global', 'launch_begin'))
+launch_end = Time.from_string(config.get('global', 'launch_end'))
 
 launch_duration = launch_end - launch_begin
 workday_duration = workday_end - workday_begin - launch_duration
 
-time_now = Time((datetime.now().hour, datetime.now().minute))
-total_tracked_time = sum((fd.get_total(), tw.get_total(), ji.get_total()))
+time_now = Time.from_string(datetime.now().strftime('%H:%M'))
+
+total_bill_time = sum((fd.get_bill(), tw.get_bill(), ji.get_bill()))
+total_free_time = sum((fd.get_free(), tw.get_free(), ji.get_free()))
+total_tracked_time = total_bill_time + total_free_time
+
 if args.offset == 0 and workday_begin <= time_now <= workday_end:
     total_time = time_now - workday_begin
     if launch_begin <= time_now <= launch_end:
@@ -249,14 +276,26 @@ if args.offset == 0 and workday_begin <= time_now <= workday_end:
     untracked_time = total_time - total_tracked_time
 else:
     untracked_time = workday_duration - total_tracked_time
-#
+
+terminal_width = 60
+bill_part = int(total_bill_time.seconds / workday_duration.seconds * terminal_width)
+free_part = int(total_free_time.seconds / workday_duration.seconds * terminal_width)
+none_part = int(untracked_time.seconds / workday_duration.seconds * terminal_width)
+
 print(f'''
-Total tracked time:    {sum((fd.get_total(), tw.get_total(), ji.get_total()))}
-- Freshdesk billable:  {fd.get_billable()}
-- Freshdesk free:      {fd.get_notbillable()}
-- Teamwork billable:   {tw.get_billable()}
-- Teamwork free:       {tw.get_notbillable()}
-- Jira:                {ji.get_notbillable()}
+
+Total tracked time:    {total_tracked_time}
+- Freshdesk billable:  {fd.get_bill()}
+- Freshdesk free:      {fd.get_free()}
+- Teamwork billable:   {tw.get_bill()}
+- Teamwork free:       {tw.get_free()}
+- Jira:                {ji.get_free()}
+
+Bill to free ratio:
+[{TermColor.GREEN + ('#' * bill_part) +
+  TermColor.NORM + ('#' * free_part) +
+  TermColor.RED + ('#' * none_part) +
+  TermColor.END}]
 
 Untracked time{' by now' if offset == 0 else ''}: {untracked_time}
 ''')
