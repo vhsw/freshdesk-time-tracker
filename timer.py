f31cf993b2e280968b7bd3e4b450854fe0141016
Alexey Dubrov <nevermind1025@gmail.com>
2018-08-31 18:45:25 +0300

    async

--- timer.py
+++ timer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 import argparse
-# import aiohttp
 import asyncio
 import configparser
 import os
@@ -9,10 +8,7 @@ from dataclasses import dataclass, field
 from datetime import datetime, date, timedelta
 from typing import List, Tuple, Dict
 
-import requests
-import urllib3
-from requests.adapters import HTTPAdapter
-from requests.auth import HTTPBasicAuth
+import aiohttp
 
 
 @dataclass(order=True)
@@ -104,8 +100,9 @@ class TicketingSystem:
     params: Dict[str, str] = field(init=False)
     url: str = field(init=False)
     api_url: str = field(init=False)
-    entries: List[Entry] = field(init=False)
     entry_url: str = field(init=False)
+    json: Dict = field(init=False)
+    entries: List = field(init=False)
     max_retries: int = field(init=False, default=5)
     timeout: int = field(init=False, default=5)
 
@@ -120,23 +117,27 @@ class TicketingSystem:
     def __repr__(self):
         return __name__ + self.__str__()
 
-    def get_json(self):
-        json = {}
-        try:
-            s = requests.Session()
-            s.mount('https://', HTTPAdapter(max_retries=self.max_retries))
-            ans = s.get(self.api_url, params=self.params, auth=self.auth, timeout=self.timeout)
-            if ans.status_code != 200:
-                raise urllib3.exceptions.ResponseError(ans.status_code, self.api_url)
-            json = ans.json()
-        except urllib3.exceptions.ConnectTimeoutError:
-            print('Connection timeout...')
-        except urllib3.exceptions.ResponseError as e:
-            print(f'Warning! Got response code {e.args[0]} on {e.args[1]}')
-        return json
+    async def get_json(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.api_url, params=self.params, timeout=self.timeout, auth=self.auth) as resp:
+                self.json = await resp.json()
 
-    def parse_json(self):
-        raise (Exception, 'Not implemented')
+        # json = {}
+        # try:
+        #     s = requests.Session()
+        #     s.mount('https://', HTTPAdapter(max_retries=self.max_retries))
+        #     ans = s.get(self.api_url, params=self.params, auth=self.auth, timeout=self.timeout)
+        #     if ans.status_code != 200:
+        #         raise urllib3.exceptions.ResponseError(ans.status_code, self.api_url)
+        #     json = ans.json()
+        # except urllib3.exceptions.ConnectTimeoutError:
+        #     print('Connection timeout...')
+        # except urllib3.exceptions.ResponseError as e:
+        #     print(f'Warning! Got response code {e.args[0]} on {e.args[1]}')
+        # self.json = json
+
+    def get_entries(self):
+        raise Exception('Not implemented')
 
     def get_bill(self):
         time = Time(sum(i.spent.seconds for i in self.entries if i.billable))
@@ -154,26 +155,27 @@ class TicketingSystem:
 class Freshesk(TicketingSystem):
     def __post_init__(self):
         self.report_date -= timedelta(hours=self.config.getint('freshdesk', 'tz_shift')) + timedelta(seconds=1)
-        self.auth = (self.config.get('freshdesk', 'api_key'), 'X')
+        self.auth = aiohttp.BasicAuth(self.config.get('freshdesk', 'api_key'), 'X')
         self.params = {'agent_id': self.config.get('freshdesk', 'agent_id'),
                        'executed_after': self.report_date.strftime('%Y-%m-%dT%H:%M:%SZ'),
                        'executed_before': (self.report_date + timedelta(days=1)).strftime('%Y-%m-%dT%H:%M:%SZ')}
         self.url = self.config.get('freshdesk', 'url')
         self.api_url = self.url + '/api/v2/time_entries'
         self.entry_url = self.url + '/a/tickets/'
-        self.json = self.get_json()
-        self.data = sorted(self.json, key=lambda k: (k.get('ticket_id'), k.get('updated_at'))) if self.json else []
+
+    def get_entries(self):
+        data = sorted(self.json, key=lambda k: (k.get('ticket_id'), k.get('updated_at'))) if self.json else []
         self.entries = [Entry(id=i.get('ticket_id'),
                               billable=i.get('billable'),
                               spent=Time.from_string(i.get('time_spent')),
                               note=i.get('note'))
-                        for i in self.data]
+                        for i in data]
 
 
 @dataclass
 class TeamWork(TicketingSystem):
     def __post_init__(self):
-        self.auth = HTTPBasicAuth(self.config.get('teamwork', 'api_key'), 'X')
+        self.auth = aiohttp.BasicAuth(self.config.get('teamwork', 'api_key'), 'x')
         self.url = self.config.get('teamwork', 'url')
         self.api_url = self.url + '/time_entries.json'
         self.entry_url = self.url + '/#tasks/'
@@ -182,13 +184,14 @@ class TeamWork(TicketingSystem):
             'fromdate': self.report_date.strftime('%Y%m%d'),
             'todate': self.report_date.strftime('%Y%m%d')
         }
-        self.json = self.get_json()
-        self.data = sorted(self.get_json().get('time-entries')) if self.json else []
+
+    def get_entries(self):
+        data = sorted(self.json.get('time-entries'), key=lambda k: (k.get('date'))) if self.json else []
         self.entries = [Entry(id=i.get('todo-item-id'),
                               spent=(Time(int(i.get('hours')) * 3600 + int(i.get('minutes')) * 60)),
                               billable=(i.get('isbillable') == 1),
                               note=i.get('project-name'))
-                        for i in self.data]
+                        for i in data]
 
 
 @dataclass
@@ -196,32 +199,41 @@ class Jira(TicketingSystem):
     def __post_init__(self):
         jira_login = self.config.get('jira', 'login')
         jira_pass = self.config.get('jira', 'password')
-        self.auth = HTTPBasicAuth(jira_login, jira_pass)
+        self.auth = aiohttp.BasicAuth(jira_login, jira_pass)
         self.url = self.config.get('jira', 'url')
         self.api_url = self.url + '/rest/api/2/search'
         self.entry_url = self.url + '/browse/'
-        json = requests.post(
-            self.api_url,
-            headers={'Content-Type': 'application/json'},
-            json={
-                'jql': f'''worklogAuthor = {jira_login} AND worklogDate = {self.report_date.strftime('%Y-%m-%d')}''',
-                'fields': ['key'],
-                'maxResults': 1000},
-            auth=HTTPBasicAuth(jira_login, jira_pass)).json()
-        entries = []
-        for issue in json['issues']:
-            ans = requests.get(f'{self.url}/rest/api/2/issue/' +
-                               issue['key'] + '/worklog',
-                               auth=HTTPBasicAuth(jira_login, jira_pass)).json()
+        self.params = {
+            'jql': f'''worklogAuthor=currentUser() and worklogDate={self.report_date.strftime('%Y-%m-%d')}''',
+            'maxResults': 1000,
+            'fields': 'id'}
+
+        self.entries = []
+
+    def get_entries(self):
+        def sync_wait(future):
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(future)
+            return  future.result()
+        async def get_issue(url):
+            with aiohttp.ClientSession as session:
+                with session.get(url, timeout=self.timeout, auth=self.auth) as resp:
+                    return await resp.json()
+        for issue in self.json['issues']:
+            url = issue['self']
+
+            ans = asyncio.run(get_issue(url))
+                # ans = requests.get(f'{self.url}/rest/api/2/issue/' +
+                #                    issue['key'] + '/worklog',
+                #                    auth=self.auth).json()
             for worklog in ans['worklogs']:
-                if (worklog['author']['name'] == jira_login and
+                if (worklog['author']['name'] == self.config.get('jira', 'login') and
                         worklog['started'].split('T')[0] == report_date.strftime('%Y-%m-%d')):
                     time_spent = int(worklog.get('timeSpentSeconds'))
-                    entries.append(Entry(id=issue.get('key'),
-                                         billable=False,
-                                         spent=Time(time_spent),
-                                         note=worklog.get('comment')))
-        self.entries = entries
+                    self.entries.append(Entry(id=issue.get('key'),
+                                              billable=False,
+                                              spent=Time(time_spent),
+                                              note=worklog.get('comment')))
 
 
 parser = argparse.ArgumentParser(description='Simple time tracker for Freshdesk')
@@ -261,85 +273,108 @@ print(f'''Time records for {report_date.strftime(f'{date_color}%a %d %b %Y{TermC
 
 params = (config, report_date, offset)
 
-print('\rGetting Freshesk...', end='')
+# print('\rGetting Freshesk...', end='')
 fd = Freshesk(*params)
-print('\rGetting Jira...    ', end='')
-ji = Jira(*params)
-print('\rGetting TeamWork...', end='')
 tw = TeamWork(*params)
-print('\r                   ', end='\r')
+ji = Jira(*params)
 
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    asyncio.wait(
+        (
+            fd.get_json(),
+            tw.get_json(),
+            ji.get_json(),
 
-def print_if_not_empty(ts: TicketingSystem):
-    if ts.entries:
-        print(ts)
+        )
+    )
+)
+loop.run_until_complete(asyncio.wait((ji.get_entries(),)))
+loop.close()
+fd.get_entries()
+tw.get_entries()
 
+print('ji', ji)
+print('fd', fd)
+print('tw', tw)
 
-print_if_not_empty(fd)
-print_if_not_empty(ji)
-print_if_not_empty(tw)
-
-workday_begin = Time.from_string(config.get('global', 'workday_begin'))
-workday_end = Time.from_string(config.get('global', 'workday_end'))
-launch_begin = Time.from_string(config.get('global', 'launch_begin'))
-launch_end = Time.from_string(config.get('global', 'launch_end'))
-
-launch_duration = launch_end - launch_begin
-workday_duration = workday_end - workday_begin - launch_duration
-
-time_now = Time.from_string(datetime.now().strftime('%H:%M'))
-
-total_bill_time = sum((fd.get_bill(), tw.get_bill(), ji.get_bill()))
-total_free_time = sum((fd.get_free(), tw.get_free(), ji.get_free()))
-total_tracked_time = total_bill_time + total_free_time
-
-if args.offset == 0 and workday_begin <= time_now <= workday_end:
-    total_time = time_now - workday_begin
-    if launch_begin <= time_now <= launch_end:
-        total_time = launch_begin - workday_begin
-    if time_now > launch_end:
-        total_time -= launch_duration
-
-    untracked_time = total_time - total_tracked_time
-else:
-    untracked_time = workday_duration - total_tracked_time
-
-# Ceil to 5 minutes
-untracked_time.seconds = untracked_time.seconds if untracked_time.seconds % 300 == 0 else untracked_time.seconds // 300 * 300 + 300
-
-
-def bill_to_free_ratio(bill_time=Time(0), free_time=Time(0), untracked=Time(0),
-                       workday_duration=Time.from_params(hours=8), terminal_width=80):
-    total = bill_time.seconds + free_time.seconds + untracked_time.seconds
-    rest_time = Time(workday_duration.seconds - total)
-    if rest_time.seconds < 0:
-        rest_time = Time(0)
-    else:
-        total = workday_duration.seconds
-
-    bill_part = int(bill_time.seconds / total * terminal_width)
-    free_part = int(free_time.seconds / total * terminal_width)
-    none_part = int(untracked.seconds / total * terminal_width)
-    rest_part = int(rest_time.seconds / total * terminal_width)
-    return f'''Bill to free ratio:
-[{TermColor.GREEN + ('#' * bill_part) +
-  TermColor.NORM +  ('#' * free_part) +
-  TermColor.RED +   ('#' * none_part) +
-  TermColor.NORM +  (' ' * rest_part)}]
-'''
-
-
-print(f'''
-Total tracked time:    {total_tracked_time}
-- Freshdesk billable:  {fd.get_bill()}
-- Freshdesk free:      {fd.get_free()}
-- Teamwork billable:   {tw.get_bill()}
-- Teamwork free:       {tw.get_free()}
-- Jira:                {ji.get_free()}
-
-{bill_to_free_ratio(total_bill_time,
-                    total_free_time,
-                    untracked_time,
-                    workday_duration)}
-Untracked time{' by now' if offset == 0 else ''}: {untracked_time}
-''')
+# print('\rGetting Jira...    ', end='')
+# ji = Jira(*params)
+# print('\rGetting TeamWork...', end='')
+# tw = TeamWork(*params)
+# print('\r                   ', end='\r')
+#
+#
+# def print_if_not_empty(ts: TicketingSystem):
+#     if ts.entries:
+#         print(ts)
+#
+#
+# print_if_not_empty(fd)
+# print_if_not_empty(ji)
+# print_if_not_empty(tw)
+#
+# workday_begin = Time.from_string(config.get('global', 'workday_begin'))
+# workday_end = Time.from_string(config.get('global', 'workday_end'))
+# launch_begin = Time.from_string(config.get('global', 'launch_begin'))
+# launch_end = Time.from_string(config.get('global', 'launch_end'))
+#
+# launch_duration = launch_end - launch_begin
+# workday_duration = workday_end - workday_begin - launch_duration
+#
+# time_now = Time.from_string(datetime.now().strftime('%H:%M'))
+#
+# total_bill_time = sum((fd.get_bill(), tw.get_bill(), ji.get_bill()))
+# total_free_time = sum((fd.get_free(), tw.get_free(), ji.get_free()))
+# total_tracked_time = total_bill_time + total_free_time
+#
+# if args.offset == 0 and workday_begin <= time_now <= workday_end:
+#     total_time = time_now - workday_begin
+#     if launch_begin <= time_now <= launch_end:
+#         total_time = launch_begin - workday_begin
+#     if time_now > launch_end:
+#         total_time -= launch_duration
+#
+#     untracked_time = total_time - total_tracked_time
+# else:
+#     untracked_time = workday_duration - total_tracked_time
+#
+# # Ceil to 5 minutes
+# untracked_time.seconds = untracked_time.seconds if untracked_time.seconds % 300 == 0 else untracked_time.seconds // 300 * 300 + 300
+#
+#
+# def bill_to_free_ratio(bill_time=Time(0), free_time=Time(0), untracked=Time(0),
+#                        workday_duration=Time.from_params(hours=8), terminal_width=80):
+#     total = bill_time.seconds + free_time.seconds + untracked_time.seconds
+#     rest_time = Time(workday_duration.seconds - total)
+#     if rest_time.seconds < 0:
+#         rest_time = Time(0)
+#     else:
+#         total = workday_duration.seconds
+#
+#     bill_part = int(bill_time.seconds / total * terminal_width)
+#     free_part = int(free_time.seconds / total * terminal_width)
+#     none_part = int(untracked.seconds / total * terminal_width)
+#     rest_part = int(rest_time.seconds / total * terminal_width)
+#     return f'''Bill to free ratio:
+# [{TermColor.GREEN + ('#' * bill_part) +
+#   TermColor.NORM +  ('#' * free_part) +
+#   TermColor.RED +   ('#' * none_part) +
+#   TermColor.NORM +  ('_' * rest_part)}]
+# '''
+#
+#
+# print(f'''
+# Total tracked time:    {total_tracked_time}
+# - Freshdesk billable:  {fd.get_bill()}
+# - Freshdesk free:      {fd.get_free()}
+# - Teamwork billable:   {tw.get_bill()}
+# - Teamwork free:       {tw.get_free()}
+# - Jira:                {ji.get_free()}
+#
+# {bill_to_free_ratio(total_bill_time,
+#                     total_free_time,
+#                     untracked_time,
+#                     workday_duration)}
+# Untracked time{' by now' if offset == 0 else ''}: {untracked_time}
+# ''')
